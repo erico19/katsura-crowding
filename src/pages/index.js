2946fd9f3ed6388148b59e1cf5f158ed07816644
Ridwan Satria <73154922+riidwansatria@users.noreply.github.com>
2022-11-18 12:23:13 +0900

    added icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,21 @@ import TrendChart from '../components/TrendChart';
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import LiveIcon from "../images/live-icon.svg"
+import Exclamation from "../images/Exclamation.svg"
+
 const IndexPage = () => (
   <Layout>
     <div className="grid grid-cols-1 p-4 gap-2">
       <div className="px-8 py-16">
         <CrowdChart />
       </div>
-      <div className="p-2 bg-yellow-200 rounded-lg">
+      <div className="flex gap-1 p-2 bg-yellow-200 rounded-lg items-center">
+        <img
+          className="inline h-4"
+          src={Exclamation}
+          alt="Exclamation"
+        />
         <p className="text-xs text-gray-800 font-medium">Expected to be less crowded in 30 minutes</p>
         
       </div>
@@ -21,7 +29,12 @@ const IndexPage = () => (
       <div className="p-4 bg-gray-100 rounded-lg">
         <h2 className="text-md text-gray-500 font-medium">Monday, October 31st 2022</h2>
         <TrendChart />
-        <div>
+        <div className="flex gap-1 items-center">
+          <img
+            className="inline h-3"
+            src={LiveIcon}
+            alt="Live"
+          />
           <span className="text-sm text-gray-800 font medium">Busier than usual</span>
         </div>
       </div>
