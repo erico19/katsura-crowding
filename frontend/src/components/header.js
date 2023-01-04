ab4b476b247d9d72c50a648c6aaf7cb6ec4a39df
StellahFoxtail <99243770+StellahFoxtail@users.noreply.github.com>
2023-01-04 20:53:36 +0900

    Update header.js

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -5,14 +5,14 @@ import kyodaiLogo from "/src/images/kyodai-logo.svg"
 const Header = () => (
   <div>    
     <div className="p-4 md:p-8 bg-primary">
-      <div className="max-w-6xl mx-auto flex justify-between items-center">
+      <div className="max-w-6xl mx-auto flex justify-center items-center">
         <img
           className="flex w-16"
           src={kyodaiLogo}
           alt="Logo"
         />
         <Link to="/">
-          <h1 className="flex text-white text-3xl font-bold text-center">Katsura Main Student Cafeteria</h1>
+          <h1 className="flex text-white text-4xl font-bold text-center">Katsura Main Student Cafeteria</h1>
         </Link>
       </div>
     </div>
