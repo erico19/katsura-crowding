57e329aca933bcd85f6c8fa1ac32db1ab94845b1
Ridwan Satria <73154922+riidwansatria@users.noreply.github.com>
2023-01-08 12:43:55 +0900

    added online survey button

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import * as React from 'react';
 
 import Layout from "./Layout"
 import DisplayChart from "./DisplayChart"
@@ -10,10 +10,17 @@ const App = () => {
       <div className="grid grid-cols-1 md:grid-cols-5 p-4 md:py-24 md:px-0 gap-2 md:gap-16 lg:gap-24 items-end max-w-6xl mx-auto">
         <div className="col-span-5 relative grid grid-cols-1 gap-2 content-start">
           <DisplayChart/>
-          <div className="flex">
-            <button className="bg-primary hover:bg-gray-600 text-white text-sm sm:text-md font-medium py-2 px-4 rounded-lg">
-              <a to="https://west2-univ.jp/sp/index.php?t=650120">Today's menu →</a>
-            </button>
+          <div className="flex gap-2">
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLScU2ZbhWCi0iWpxQf8SrkZn0OopxQ14FeHZs0MaDCwGQ1qmrg/viewform?usp=sf_link">
+              <button className="bg-primary hover:bg-gray-600 text-white text-sm sm:text-md font-medium py-2 px-4 rounded-lg">
+                Online survey →
+              </button>
+            </a>
+            <a href="https://west2-univ.jp/sp/index.php?t=650120">
+              <button className="hover:bg-gray-100 border-2 border-gray-200 text-primary text-sm sm:text-md font-medium py-2 px-4 rounded-lg">
+                Today's menu →
+              </button>
+            </a>
           </div>
         </div>
       </div>
