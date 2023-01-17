0c9390fd8a1afbba017cccd48c3ff2a5c60be3e8
Ridwan Satria <73154922+riidwansatria@users.noreply.github.com>
2023-01-17 12:40:49 +0900

    added a separate admin page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,13 +3,13 @@ import * as React from 'react';
 import Layout from "./Layout"
 import DisplayChart from "./DisplayChart"
 
-const App = () => {
+const App = ({ admin }) => {
   return (
     <Layout>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
       <div className="grid grid-cols-1 md:grid-cols-5 p-4 md:py-24 md:px-0 gap-2 md:gap-16 lg:gap-24 items-end max-w-6xl mx-auto">
         <div className="col-span-5 relative grid grid-cols-1 gap-2 content-start">
-          <DisplayChart/>
+          <DisplayChart admin={admin}/>
           <div className="flex gap-2">
             <a href="https://docs.google.com/forms/d/e/1FAIpQLScU2ZbhWCi0iWpxQf8SrkZn0OopxQ14FeHZs0MaDCwGQ1qmrg/viewform?usp=sf_link">
               <button className="bg-primary hover:bg-gray-600 text-white text-sm sm:text-md font-medium py-2 px-4 rounded-lg">
---
+++ frontend/src/pages/admin.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import App from '../components/App';
+
+const AdminPage = () => (
+  <App admin="TRUE" />
+)
+
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
+
+export default AdminPage
