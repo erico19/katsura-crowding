89bed90d0cf781f7d0b55fd8ca1f98d3da612c91
Ridwan Satria <73154922+riidwansatria@users.noreply.github.com>
2023-01-25 22:13:08 +0900

    updated location names

--- frontend/src/components/DisplayChart.js
+++ frontend/src/components/DisplayChart.js
@@ -16,12 +16,12 @@ function MyDropdown({admin}) {
         <div classname='flex'>
           <select value={selectedOption} onChange={handleChange}>
             <option value="AMPM18-KJ008">C Cluster Admnistration Office</option>
-            <option value="AMPM18-KJ009">C1 Entrance</option>
+            <option value="AMPM18-KJ009">C1 Building Entrance</option>
             <option value="AMPM18-KJ010">Inside Main Cafeteria</option>
             <option value="AMPM18-KJ011">B Cluster Administration Office</option>
-            <option value="AMPM18-KJ012">Katsura Library</option>
-            <option value="AMPM18-KJ013">A1 Entrance</option>
-            <option value="AMPM18-KJ014">A2 Park Mae</option>
+            <option value="AMPM18-KJ012">Katsura Main Library</option>
+            <option value="AMPM18-KJ013">A4 Rear Entrance</option>
+            <option value="AMPM18-KJ014">P3P Parking Mae</option>
             <option value="AMPM18-KJ015">Boulangerie Seri Bakery</option>
             <option value="AMPM18-KJ016">Main Cafeteria Entrance</option>
             <option value="AMPM18-KJ017">Main Cafeteria Exit</option>
